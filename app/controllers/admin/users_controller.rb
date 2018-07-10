53a6e7f7cd53c6e2f9993c53956c53d2601449e4
Yulia Oletskaya <yulia.oletskaya@gmail.com>
2018-07-10 18:43:22 +0300

    Identify admins on edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,8 @@
 class Admin::UsersController < ApplicationController
   before_action :authorize_access_request!
   before_action :set_user, only: [:show, :update]
-  ROLES = %w[admin manager].freeze
+  VIEW_ROLES = %w[admin manager].freeze
+  EDIT_ROLES = %w[admin].freeze
 
   def index
     @users = User.all
@@ -14,18 +15,27 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    @user.update!(user_params)
-    render json: @user.as_json(only: [:id, :email, :role])
+    if current_user.id != @user.id
+      @user.update!(user_params)
+      JWTSessions::Session.new(namespace: "user_#{@user.id}").flush_namespaced_access_tokens
+      render json: @user.as_json(only: [:id, :email, :role])
+    else
+      render json: { error: 'Admin cannot modify their own role' }, status: :bad_request
+    end
   end
 
   def token_claims
     {
-      aud: ROLES,
+      aud: allowed_aud,
       verify_aud: true
     }
   end
 
   private
+
+  def allowed_aud
+    action_name == 'update' ? EDIT_ROLES : VIEW_ROLES
+  end
 
   def set_user
     @user = User.find(params[:id])
