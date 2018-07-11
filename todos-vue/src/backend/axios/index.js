b7878a596d76d1eddfdcc51758aa9c0da8b42352
Yulia Oletskaya <yulia.oletskaya@gmail.com>
2018-07-11 11:06:06 +0300

    Fetch current user after refresh

--- todos-vue/src/backend/axios/index.js
+++ todos-vue/src/backend/axios/index.js
@@ -35,10 +35,11 @@ securedAxiosInstance.interceptors.response.use(null, error => {
     // In case 401 is caused by expired access cookie - we'll do refresh request
     return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': store.state.csrf } })
       .then(response => {
-        store.commit('refresh', response.data.csrf)
+        plainAxiosInstance.get('/me')
+          .then(meResponse => store.commit('setCurrentUser', { currentUser: meResponse.data, csrf: response.data.csrf }))
         // And after successful refresh - repeat the original request
         let retryConfig = error.response.config
-        retryConfig.headers['X-CSRF-TOKEN'] = store.state.csrf
+        retryConfig.headers['X-CSRF-TOKEN'] = response.data.csrf
         return plainAxiosInstance.request(retryConfig)
       }).catch(error => {
         store.commit('unsetCurrentUser')
